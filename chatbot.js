c7c383a10edb31936d42f2eb3993c3eee53139da
Adarshpandey-007 <adarshpandey6364@gmail.com>
2024-10-26 01:51:43 +0530

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -8,7 +8,7 @@
     const closeChatbot = document.querySelector("#close-chatbot");
 
     // API setup
-    const API_KEY = "AIzaSyDbUHuUGdBR-vCMQj7FIMBgLXUaBVaGwDE";
+    const API_KEY = "AIzaSyCw3PqpHFJqmf-bkpIvvEyFSmUx6WvNI14";
     const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
     // Initialize user message and file data
